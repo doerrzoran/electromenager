47cc6a875cfa2fcb832b427ef7007d33a6f93cc5
zozo222 <doerrzoran@gmail.com>
2022-09-27 15:45:43 +0200

    creation de database

--- database/CreateDatabase.sql
+++ database/CreateDatabase.sql
@@ -1,1 +1,6 @@
-CREATE DATABASE IF NOT EXISTS electromenager_bdd;
+-- CREATE DATABASE electromenager_bdd;
+
+CREATE TABLE IF NOT EXISTS User_Ref(
+  id serial PRIMARY KEY,
+  Label VARCHAR(250) NOT NULL
+);
