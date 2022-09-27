1b0ff4467179dd494cadfffa0298ca845a53853c
zozo222 <doerrzoran@gmail.com>
2022-09-27 16:25:36 +0200

    create table user

--- database/CreateDatabase.sql
+++ database/CreateDatabase.sql
@@ -4,3 +4,13 @@ CREATE TABLE IF NOT EXISTS User_Ref(
   id serial PRIMARY KEY,
   Label VARCHAR(250) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS User(
+  id serial PRIMARY KEY,
+  name VARCHAR (250) NOT NULL,  
+  firstname VARCHAR (250) NOT NULL,  
+  mail VARCHAR (250) NOT NULL,  
+  password VARCHAR (250) NOT NULL,  
+  role INTEGER NOT NULL,
+  FOREIGN KEY(role) REFERENCES User_Ref(id)
+);
