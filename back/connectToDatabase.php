d2069e58bdf87b18fc59c19a60956ea4256c3910
zozo222 <doerrzoran@gmail.com>
2022-09-28 17:01:52 +0200

    re

--- back/connectToDatabase.php
+++ back/connectToDatabase.php
@@ -6,16 +6,16 @@ $infobdd = $_POST;
 $dbhost = $infobdd["Host"];
 
 $dbname = $infobdd["Database"];
-echo $dbname" ";
+echo $dbname." ";
 $dbuser = $infobdd["User"];
-echo $dbuser" ";
+echo $dbuser." ";
 $dbport = $infobdd["Port"];
-echo $dbport" ";
+echo $dbport." ";
 $dbpassword = $infobdd["Password"];
-echo $dbpassword" ";
+echo $dbpassword." ";
 
 
-$conn = pg_connect("host=$dbhost dbname=$dbname user=$dbuser port=$dbport password=$dbpassword");
+$conn = pg_connect("host=".$dbhost." dbname=".$dbname." user=".$dbuser." port=".$dbport." password=".$dbpassword.");
 
 if(!$conn){
     die('Connection error: '). pg_connect_error();
