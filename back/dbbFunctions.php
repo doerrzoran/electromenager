560029fe57a91f086d7fc15c676966817ace3191
zozo222 <doerrzoran@gmail.com>
2022-09-28 17:48:49 +0200

    dbbfunction

---
+++ back/dbbFunctions.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once "connectToDatabase.php";
+ 
+ function selectFromDatabase($table, $key, $data, $conn){
+    $query = "SELECT * FROM $table WHERE $key = '$data'";
+    $result = pg_query($conn, $query);
+    $row = pg_fetch_row($result);
+    return $row;
+  }
+
+
+  function insertintoDatabase($table, $key, $data, $conn){
+    $query = "INSERT INTO $table ($key) VALUES ($data)";
+    $result = pg_query($conn, $query);
+    return $result;
+  } 
