35045fc60400ceb78e05879fb6fba320558b7b39
zozo222 <doerrzoran@gmail.com>
2022-09-29 11:16:17 +0200

    modification bdd

--- back/connectToDatabase.php
+++ back/connectToDatabase.php
@@ -1,13 +1,13 @@
 <?php
 
-$infobdd = $_POST;
 
 
-$dbhost = $infobdd["Host"];
-$dbname = $infobdd["Database"];
-$dbuser = $infobdd["User"];
-$dbport = $infobdd["Port"];
-$dbpassword = $infobdd["Password"];
+
+$dbhost = "ec2-18-209-78-11.compute-1.amazonaws.com";
+$dbname = "d2kdl93b5pjda";
+$dbuser = "osfvxyrzqmyqvx";
+$dbport = "5432";
+$dbpassword = "eff02eb83a59b0ff2eb481b4435da7b1db14b23a5ea55dbe8f943b2c86692814";
 
 
 $conn = pg_connect("host=".$dbhost." dbname=".$dbname." user=".$dbuser." port=".$dbport." password=".$dbpassword."");
--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <html>
     <body>
         <div>electromenager</div>
-        <a href="front\connectionbdd.html">connecter à une base de données</a>
         <a href="back\scriptConnexion.php">verification de compte</a>
     </body>
 </html>
