9c77db588a7d94cb8850adacbc9ff705fad1d294
zozo222 <doerrzoran@gmail.com>
2022-09-29 10:27:41 +0200

    re

--- index.php
+++ index.php
@@ -2,5 +2,6 @@
     <body>
         <div>electromenager</div>
         <a href="front\connectionbdd.html">connecter à une base de données</a>
+        <a href="back\scriptConnexion.php">verification de compte</a>
     </body>
 </html>
