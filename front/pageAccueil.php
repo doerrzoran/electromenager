627af1a179fa1b43140a1e33b90e6c00b85abfdd
zozo222 <doerrzoran@gmail.com>
2022-10-04 14:53:21 +0200

    re

--- front/pageAccueil.php
+++ front/pageAccueil.php
@@ -4,12 +4,7 @@
             <?php
             require_once "welcomeUser.php";
             ?>
-            <h3><?php
-            if(!$userName){
-                welcomeUser($userName, $userFirstname); 
-            }
-            
-            ?></h3>
+            <h3><?php welcomeUser($userName, $userFirstname); ?></h3>
         </div>
         <a href="creationDeCompte.html">creer un compte</a>
         <a href="connexion.html">se connecter</a>
