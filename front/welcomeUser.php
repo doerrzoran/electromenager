9102f8d4d37ce3ca96e4326819b1a56cf84b9a58
zozo222 <doerrzoran@gmail.com>
2022-10-04 14:45:00 +0200

    re

--- front/welcomeUser.php
+++ front/welcomeUser.php
@@ -4,5 +4,7 @@ require_once '../back/userId.php';
 // echo $userName;
 
 function welcomeUser($userName, $userFirstname){
-    echo "connecté en temps que ".$userName." ".$userFirstname;
+    if(!$userName){
+        echo "connecté en temps que ".$userName." ".$userFirstname;
+    }
 }
