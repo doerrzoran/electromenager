af4cec098ab699ca1a11ae797a1ccaf198dfd4bf
zozo222 <doerrzoran@gmail.com>
2022-10-04 11:35:52 +0200

    re

--- back/userData.php
+++ back/userData.php
@@ -1,14 +1,14 @@
 <?php
-
+var_dump($users);
 session_start();
-    foreach($users as $user){
-        var_dump($user);
-        $_SESSION['id'] = $user['id'];
-        $_SESSION['name'] = $user['name'];
-        $_SESSION['firstname'] = $user['firstname'];
-        $_SESSION['mail'] = $user['mail'];
-        $_SESSION['password'] = $user['password'];
-    } 
+foreach($users as $user){
+    var_dump($user);
+    $_SESSION['id'] = $user['id'];
+    $_SESSION['name'] = $user['name'];
+    $_SESSION['firstname'] = $user['firstname'];
+    $_SESSION['mail'] = $user['mail'];
+    $_SESSION['password'] = $user['password'];
+} 
 
 
 
