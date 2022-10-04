3ffec3245a352f13b2774b909e64348522358e3c
zozo222 <doerrzoran@gmail.com>
2022-10-04 14:51:28 +0200

    re

--- front/welcomeUser.php
+++ front/welcomeUser.php
@@ -1,5 +1,5 @@
 <?php
-require_once '../back/userId.php';
+require_once '../back/userId.php'; 
 
 function welcomeUser($userName, $userFirstname){
     echo "connecté en temps que ".$userName." ".$userFirstname;
