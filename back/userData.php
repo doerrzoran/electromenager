7f463baeaa363d01a4d134526b411a19c1168993
zozo222 <doerrzoran@gmail.com>
2022-10-04 14:14:36 +0200

    re

--- back/userData.php
+++ back/userData.php
@@ -8,7 +8,7 @@ $_SESSION['firstname'] = $user[2];
 $_SESSION['mail'] = $user[3];
 $_SESSION['password'] = $user[4];
 
+echo $_SESSION['name'];
 
 
-
-header('Location: ../front/pageAccueil.php');
+// header('Location: ../front/pageAccueil.php');
