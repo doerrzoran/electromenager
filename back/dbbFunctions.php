5ede9d455d1b229494b2e2bf6456fba000f4de91
zozo222 <doerrzoran@gmail.com>
2022-10-10 11:54:51 +0200

    re

--- back/dbbFunctions.php
+++ back/dbbFunctions.php
@@ -10,7 +10,7 @@ require_once "connectToDatabase.php";
   }
 
   function selectFromDatabaseAnd($table, $key1, $key2, $data1, $data2 $conn){
-    $query = "SELECT * FROM $table WHERE $key1 = '$data1' AND $key2 = '$data2";
+    $query = "SELECT * FROM $table WHERE $key1 = '$data1' AND $key2 = '$data2'";
     $result = pg_query($conn, $query);
     $row = pg_fetch_row($result);
     return $row;
