a54be4fd8fd9c1315e1d5a9f44646d7b19bfc691
zozo222 <doerrzoran@gmail.com>
2022-10-10 10:12:13 +0200

    re

--- back/scriptConnexion.php
+++ back/scriptConnexion.php
@@ -21,10 +21,16 @@ $user = selectFromDatabase("User_acount", "mail", $mail, $conn);
 if($user == true){
   require 'userData.php';
 }else{
-  ?><script>var test='<?php echo $e->getMessage(); ?>'; 
-  alert(test);
-  window.location.href = "../front/connexion.html";
-  </script> <?php
+  header('Location: ../front/connexion.html'); 
 }
+
+// if($user == true){
+//   require 'userData.php';
+// }else{
+//   ?><script>var test='<?php echo $e->getMessage(); ?>'; 
+//   alert(test);
+//   window.location.href = "../front/connexion.html";
+//   </script> <?php
+// }
 
  
