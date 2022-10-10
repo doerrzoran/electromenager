3c656958b62031461ea4c2276ec9cb6a20268ac8
zozo222 <doerrzoran@gmail.com>
2022-10-10 10:25:00 +0200

    re

--- back/scriptConnexion.php
+++ back/scriptConnexion.php
@@ -9,14 +9,20 @@ $infoConnexion = $_POST;
 $mail = $infoConnexion['mail'];
 $password = $infoConnexion['password'];
 
+}else{
+  require 'scriptCreationDeCompte.php';
+}
+
 
 $user = selectFromDatabase("User_acount", "mail", $mail, $conn);
+
 
 if($user == true){
   require 'userData.php';
 }else{
   header('Location: ../front/connexion.html'); 
 }
+ 
 
 
  
