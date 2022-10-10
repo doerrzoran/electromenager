8bc05f82c3a631140d26619250d864223c219977
zozo222 <doerrzoran@gmail.com>
2022-10-10 16:28:52 +0200

    re

--- back/scriptCreationDeCompte.php
+++ back/scriptCreationDeCompte.php
@@ -4,18 +4,18 @@ require_once 'dbbFunctions.php';
 if(isset($_POST)){
 
   $infoNewAccount = $_POST;
-  var_dump($infoNewAccount);
+  
   $name = $infoNewAccount['nom'];
   $firstName = $infoNewAccount['prenom'];
   $mail = $infoNewAccount['mail'];
   $password = $infoNewAccount['password'];
   $password2 = $infoNewAccount['password2'];
 
-if($password != $password2){
-  die('le mot de passe ne correspond pas');
-}else{
-   $acount = insertIntoDatabase('user-acount', 'name, firstname, mail, password, role', '$name, $firstName, $mail, $password, 2', $conn);
+if($password === $password2){
+  $acount = insertIntoDatabase('user-acount', 'name, firstname, mail, password, role', '$name, $firstName, $mail, $password, 2', $conn);
   var_dump($acount);
+}else{
+    die('le mot de passe ne correspond pas');
 }
 
 };
