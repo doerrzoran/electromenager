358b0a8ff24689f46587d9c28784444cd73dddf5
zozo222 <doerrzoran@gmail.com>
2022-10-11 11:20:00 +0200

    re

--- back/scriptCreationDeCompte.php
+++ back/scriptCreationDeCompte.php
@@ -11,10 +11,8 @@ if(isset($_POST)){
   $password = $infoNewAccount['password'];
   $password2 = $infoNewAccount['password2'];
 
-  $newUser = "$name, $firstName, $mail, $password, 2";
-
 if($password === $password2){
-  $acount = insertIntoDatabase("user-acount", 'name, firstname, mail, password, role',  $newUser, $conn);
+  $acount = insertIntoDatabase('user-acount', 'name, firstname, mail, password, role', "'$name', '$firstName', '$mail', '$password', 2", $conn);
   var_dump($acount);
 }else{
     die('le mot de passe ne correspond pas');
