2cca608f74168bb98a297d84c81883e18250ffbf
zozo222 <doerrzoran@gmail.com>
2022-10-12 11:49:31 +0200

    re

--- front/pageAccueil.php
+++ front/pageAccueil.php
@@ -6,8 +6,6 @@
             ?>
             <h3><?php welcomeUser($userName, $userFirstname); ?></h3>
         </div>
-        <a href="creationDeCompte.html">creer un compte</a>
-        <!-- <a href="connexion.html">se connecter</a> -->
         <div class="event" id="creationDeCompte">creer un compte</div>
         <div class="event" id="connexion">se connecter</div>
         <script src ="../Javascript/redirect.js"></script>
