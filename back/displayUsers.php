cf149a0b35e809bb012c699f71db11024731b3f5
zozo222 <doerrzoran@gmail.com>
2022-10-13 15:29:30 +0200

    re

--- back/displayUsers.php
+++ back/displayUsers.php
@@ -5,6 +5,6 @@ require_once "dbbFunctions.php";
 $users = selectAllFromDatabase("user_acount", $conn);
 
 foreach($users as $user){
-    echo $user['name'].'</br>;
+    echo $user['name'].'</br>';
 }
 
