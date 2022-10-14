f8b7b3eaa7a5a9db3bafe13516640f4b6839faea
zozo222 <doerrzoran@gmail.com>
2022-10-14 13:26:13 +0200

    re

--- back/displayUsers.php
+++ back/displayUsers.php
@@ -3,9 +3,10 @@
 require_once "dbbFunctions.php";
 
 $users = selectAllFromDatabase("user_acount", $conn);
-
+echo $users;
 ?>
 
-<script> var infoUsers = "<?php echo $users ?>";</script>
+<script> var infoUsers = "<?php echo $users ?>";
+document.writeln(infoUsers);</script>
 <script src = "../Javascript/redirectUsersInfo.js"></script>
-<script>window.location.href = "../front/listeUtilisateurs.php";</script>
+<!-- <script>window.location.href = "../front/listeUtilisateurs.php";</script> -->
