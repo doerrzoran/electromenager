7bd70cd1abcd01c6a33ef77f007142eec85c17c9
zozo222 <doerrzoran@gmail.com>
2022-10-14 12:02:41 +0200

    re

--- back/displayUsers.php
+++ back/displayUsers.php
@@ -4,10 +4,6 @@ require_once "dbbFunctions.php";
 
 $users = selectAllFromDatabase("user_acount", $conn);
 
-foreach($users as $user){
-  $user['name'].'-';
-  $user['firstname'].'-';
-  $user['mail'].'-';
-  $user['password'].'-';
-}
+?>
 
+<script src = "../Javascript/redirectUsersInfo.js"></script>
