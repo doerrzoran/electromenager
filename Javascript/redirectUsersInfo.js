6a9601f6638c02c3536116bd29b9eaec9e8fa986
zozo222 <doerrzoran@gmail.com>
2022-10-14 14:14:09 +0200

    re

--- Javascript/redirectUsersInfo.js
+++ Javascript/redirectUsersInfo.js
@@ -1,7 +1,4 @@
 if(infoUsers){
-    document.writeln(infoUsers);
     sessionStorage.setItem('userInfo', infoUsers);
-    var data = sessionStorage.getItem('userInfo');
-    document.writeln(data);
  }
  
--- back/displayUsers.php
+++ back/displayUsers.php
@@ -18,3 +18,18 @@ foreach($users as $user){
 document.writeln(infoUsers);</script>
 <script src = "../Javascript/redirectUsersInfo.js"></script>
 <!-- <script>window.location.href = "../front/listeUtilisateurs.php";</script> -->
+
+<script>var data = sessionStorage.getItem('userInfo');</script>
+
+<?php
+   $users = '<script>document.writeln(data);</script>';
+   print_r($users);
+foreach($users as $user){
+    print_r($user);
+    echo $user['name'].'-';
+    echo $user['firstname'].'-';
+    echo $user['mail'].'-';
+    echo $user['password'].'-';
+}
+
+?> 
