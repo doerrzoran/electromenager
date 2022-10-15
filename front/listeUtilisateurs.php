b747c94bb9eadc4c4669082a262f0dd58b5c09d7
zozo222 <doerrzoran@gmail.com>
2022-10-15 16:47:30 +0200

    re

--- front/listeUtilisateurs.php
+++ front/listeUtilisateurs.php
@@ -1,7 +1,7 @@
 <script>
 var data = sessionStorage.getItem('Cle');
 document.write(data);
-document.write(data[0]);
-// var newArr = JSON.parse(data);
+var newArr = JSON.parse(data);
+document.write(newArr);
 </script>
 
