83db4603e838332a4dbaa068b2ad52a17e2f9033
zozo222 <doerrzoran@gmail.com>
2022-10-15 17:07:05 +0200

    re

--- back/displayUsers.php
+++ back/displayUsers.php
@@ -4,15 +4,10 @@ require_once "dbbFunctions.php";
 
 $users = selectAllFromDatabase("user_acount", $conn);
 
-foreach($users as $user){
-    ?>
-        <script>
-        var arr = <?php echo json_encode($user); ?>;
-        </script>
-    <?php
-}
-
 ?>
 
+<script>
+var arr = <?php echo json_encode($users); ?>;
+</script>
 <script src="../Javascript/redirectUsersInfo.js"></script>
 <script> window.location.href = "../front/listeUtilisateurs.php" </script>
