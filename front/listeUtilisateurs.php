2296a38e4792aa2ba33d58d36415adf39b1ef2fb
zozo222 <doerrzoran@gmail.com>
2022-10-17 14:10:48 +0200

    re

--- front/listeUtilisateurs.php
+++ front/listeUtilisateurs.php
@@ -9,12 +9,13 @@ $form = '<form action="../back/deleteUser.php" method="POST" enctype="multipart/
 ?>
 
 <script>
+var form = "<?php $form ?>";
 var data = sessionStorage.getItem('Cle');
 var newArr = JSON.parse(data);
 function tab(newArr){
   for(let i = 0; i < newArr.length; i++){
     var arr = newArr[i];
-    document.write( arr['name'] + '\n' + arr['firstname'] + '\n' + arr['mail'] + '\n' + arr['password'] + '<br>');
+    document.write( arr['name'] + '\n' + arr['firstname'] + '\n' + arr['mail'] + '\n' + arr['password'] + '\n' + form  + '<br>');
   }
   var id = arr['id']; 
   document.getElementById("input").value = id;
