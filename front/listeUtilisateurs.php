7b101d77fc0f495c7f4d712ebd590fe588d14ce5
zozo222 <doerrzoran@gmail.com>
2022-10-17 15:29:03 +0200

    re

--- front/listeUtilisateurs.php
+++ front/listeUtilisateurs.php
@@ -4,16 +4,18 @@
 
 
 <script>
+function displayUser(arr){
+    document.write(arr['id'] + '\n' + arr['name'] + '\n' + arr['firstname'] + '\n' + arr['mail'] + '\n' + arr['password'] +'\n' + form + '<br>');
+    var id = arr['id']; 
+    document.getElementById("id").value = id;
+}
 var data = sessionStorage.getItem('Cle');
 var newArr = JSON.parse(data);
 function tab(newArr){
   for(let i = 0; i < newArr.length; i++){
     var arr = newArr[i];
-    document.write(arr['id'] + '\n' + arr['name'] + '\n' + arr['firstname'] + '\n' + arr['mail'] + '\n' + arr['password'] +'\n' + form + '<br>');
-    var id = arr['id']; 
-    document.getElementById("id").value = id;
-  }
-  
+    displayUser(arr);
+  } 
 }
 
 
