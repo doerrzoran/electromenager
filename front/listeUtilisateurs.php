dfa56595605053febca605abc34c73da065cf0c7
zozo222 <doerrzoran@gmail.com>
2022-10-17 13:59:07 +0200

    création systeme effacement d'utilisateur

--- front/listeUtilisateurs.php
+++ front/listeUtilisateurs.php
@@ -1,10 +1,22 @@
+<?php
+
+
+$form = '<form action="../back/deleteUser.php" method="POST" enctype="multipart/form-data">
+    <input id ="input" type="hidden" name="input" value="Swag"</script>">
+    <button type ="submit">submit</button>
+</form>';
+
+?>
+
 <script>
 var data = sessionStorage.getItem('Cle');
 var newArr = JSON.parse(data);
 function tab(newArr){
   for(let i = 0; i < newArr.length; i++){
     var arr = newArr[i];
-    document.write(arr['id'] + '\n' + arr['name'] + '\n' + arr['firstname'] + '\n' + arr['mail'] + '\n' + arr['password'] + '<br>');
+    document.write( arr['name'] + '\n' + arr['firstname'] + '\n' + arr['mail'] + '\n' + arr['password'] + '<?php $form ?>' + '\n' + '<?php echo $form ?>' + '<br>');
+    var id = arr['id']; 
+    document.getElementById("input").value = id;
   }
 }
 
