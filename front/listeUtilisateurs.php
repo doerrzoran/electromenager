402f23e7126f559381a70d736c504bf7076355eb
zozo222 <doerrzoran@gmail.com>
2022-10-17 14:02:41 +0200

    re

--- front/listeUtilisateurs.php
+++ front/listeUtilisateurs.php
@@ -14,7 +14,7 @@ var newArr = JSON.parse(data);
 function tab(newArr){
   for(let i = 0; i < newArr.length; i++){
     var arr = newArr[i];
-    document.write( arr['name'] + '\n' + arr['firstname'] + '\n' + arr['mail'] + '\n' + arr['password'] + '<?php $form ?>' + '\n' + '<?php echo $form ?>' + '<br>');
+    document.write( arr['name'] + '\n' + arr['firstname'] + '\n' + arr['mail'] + '\n' + arr['password'] + '<?php $form ?>' + '<br>');
     var id = arr['id']; 
     document.getElementById("input").value = id;
   }
