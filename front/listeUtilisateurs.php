4115f81d24053c4f3becedc7db9697c777da92ab
zozo222 <doerrzoran@gmail.com>
2022-10-17 14:24:53 +0200

    re

--- front/listeUtilisateurs.php
+++ front/listeUtilisateurs.php
@@ -2,20 +2,20 @@
 
 
 $form = '<form action="../back/deleteUser.php" method="POST" enctype="multipart/form-data">
-    <input id ="input" type="hidden" name="input" value="Swag">
+    <input id="input" type="hidden" name="input" value="Swag">
     <button type ="submit">submit</button>
 </form>';
 
 ?>
-<script>var form = "<?php echo $form ?>";</script>
 
 <script>
+var form = "<?php echo $form ?>";
 var data = sessionStorage.getItem('Cle');
 var newArr = JSON.parse(data);
 function tab(newArr){
   for(let i = 0; i < newArr.length; i++){
     var arr = newArr[i];
-    document.write( arr['name'] + '\n' + arr['firstname'] + '\n' + arr['mail'] + '\n' + arr['password'] + '\n' + form + '<br>');
+    document.write( arr['name'] + '\n' + arr['firstname'] + '\n' + arr['mail'] + '\n' + arr['password'] + '<br>');
   }
   var id = arr['id']; 
   document.getElementById("input").value = id;
