99cc277873ecda516e2428a9834beac5ba731e92
zozo222 <doerrzoran@gmail.com>
2022-10-17 18:29:27 +0200

    re

--- front/listeUtilisateurs.php
+++ front/listeUtilisateurs.php
@@ -1,6 +1,6 @@
 
 
-<script>var form = '<form action="../back/deleteUser.php" method="POST" enctype="multipart/form-data"><input id="id" type="hidden" name="id" value="<script>document.write(id)</script>"><button type ="submit">submit</button></form>';</script>
+<script>var form = '<form action="../back/deleteUser.php" method="POST" enctype="multipart/form-data"><input id="id" type="hidden" name="id" value="<?php echo $id ?>"><button type ="submit">submit</button></form>';</script>
 
 
 <script>
@@ -10,7 +10,7 @@ function tab(newArr){
   for(let i = 0; i < newArr.length; i++){
     var arr = newArr[i];
     var id = arr['id']; 
-     
+     <?php $id = '<script>document.write(id)</script>' ?>
     document.write(arr['id'] + '\n' + arr['name'] + '\n' + arr['firstname'] + '\n' + arr['mail'] + '\n' + arr['password'] + '\n' + form + '<br>');
   }
   
