cd8829fb393dffba53951ee01ccb77df569fbd54
zozo222 <doerrzoran@gmail.com>
2022-10-17 10:32:22 +0200

    re

--- front/listeUtilisateurs.php
+++ front/listeUtilisateurs.php
@@ -4,7 +4,7 @@ var newArr = JSON.parse(data);
 function tab(newArr){
   for(let i = 0; i < newArr.length; i++){
     var arr = newArr[i];
-    document.write(arr['name'])
+    document.write(arr['name'].' 'arr['name'].' ')
   }
 }
 
