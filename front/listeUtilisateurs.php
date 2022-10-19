33eee58fbdb37d8535d8fb59ac092efcb3fe129d
zozo222 <doerrzoran@gmail.com>
2022-10-19 11:12:35 +0200

    re

--- front/listeUtilisateurs.php
+++ front/listeUtilisateurs.php
@@ -10,10 +10,9 @@ function tab(newArr){
   for(let i = 0; i < newArr.length; i++){
     var arr = newArr[i];
     var id = arr['id']; 
-    document.write(arr['id'] + '\n' + arr['name'] + '\n' + arr['firstname'] + '\n' + arr['mail'] + '\n' + arr['password'] + '<br>');
-    var form = '<form action="../back/deleteUser.php" method="POST" enctype="multipart/form-data"><input id="id" type="hidden" name="id" value="' + varr['id'] +  '"><button type ="submit">submit</button></form>';
-    document.write(form);
-  }
+    var form = '<form action="../back/deleteUser.php" method="POST" enctype="multipart/form-data"><input id="id" type="hidden" name="id" value="' + varr['id'] + '"><button type ="submit">submit</button></form>';
+    document.write(arr['id'] + '\n' + arr['name'] + '\n' + arr['firstname'] + '\n' + arr['mail'] + '\n' + arr['password'] + '\n' +  form + '<br>');
+  };
   
 }
 
