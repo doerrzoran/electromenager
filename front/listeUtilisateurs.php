bc8cadc0609c5b1eb99b67e502afaff756e020fb
zozo222 <doerrzoran@gmail.com>
2022-10-19 14:54:34 +0200

    re

--- front/listeUtilisateurs.php
+++ front/listeUtilisateurs.php
@@ -1,32 +1,21 @@
 
 <html>
   <body>
-    <div><?php require_once "header.html"; ?></div>
+    <?php require_once "header.html";  $variable = "<script>tab(newArr);</script>";
+    echo $variable;
+    ?>
   </body>
+  <script>
+  var data = sessionStorage.getItem('Cle');
+  var newArr = JSON.parse(data);
+  function tab(newArr){
+    for(let i = 0; i < newArr.length; i++){
+      var arr = newArr[i];
+      var id = arr['id']; 
+      var form = '<form action="../back/deleteUser.php" method="POST" enctype="multipart/form-data"><input id="id" type="hidden" name="id" value="' + arr['id'] + '"><button type ="submit">suppprimer cet utilisateur</button></form>';
+      document.write(arr['id'] + '\n' + arr['name'] + '\n' + arr['firstname'] + '\n' + arr['mail'] + '\n' + arr['password'] + '\n' +  form + '<br>');
+    };
+  }
+  </script>
+  <script src ="../Javascript/redirect.js"></script>
 </html>
-
-
-
-<script>
-var data = sessionStorage.getItem('Cle');
-var newArr = JSON.parse(data);
-function tab(newArr){
-  for(let i = 0; i < newArr.length; i++){
-    var arr = newArr[i];
-    var id = arr['id']; 
-    var form = '<form action="../back/deleteUser.php" method="POST" enctype="multipart/form-data"><input id="id" type="hidden" name="id" value="' + arr['id'] + '"><button type ="submit">suppprimer cet utilisateur</button></form>';
-    document.write(arr['id'] + '\n' + arr['name'] + '\n' + arr['firstname'] + '\n' + arr['mail'] + '\n' + arr['password'] + '\n' +  form + '<br>');
-  };
-  
-}
-
-
-
-</script>
-
-
-<?php $variable = "<script>tab(newArr);</script>";
-
-echo $variable;
-
-
