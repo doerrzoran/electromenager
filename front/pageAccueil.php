a2a71897cd2c399dde3a1688af1f128cea2bc5af
zozo222 <doerrzoran@gmail.com>
2022-10-20 13:14:56 +0200

    re

--- front/pageAccueil.php
+++ front/pageAccueil.php
@@ -16,7 +16,7 @@
         <script>
             var role = sessionStorage.getItem('Role');
             console.log(role);
-            if(role == 1){
+            if(role > 0){
               document.getElementById("creationDeCompte").style.display = "none";
               document.getElementById("connexion").style.display = "none";
             }else{
