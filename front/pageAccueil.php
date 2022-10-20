141381a4d5a6feeb52e2c27dcbaf2dbb715d92f2
zozo222 <doerrzoran@gmail.com>
2022-10-20 11:58:46 +0200

    re

--- front/pageAccueil.php
+++ front/pageAccueil.php
@@ -8,16 +8,15 @@
             ?>
             <?php welcomeUser($userName, $userFirstname); ?>
         </div>
-        <div class="event" class= "connecteur" id="creationDeCompte">creer un compte</div>
-        <div class="event" class= "connecteur" id="connexion">se connecter</div>
+        <div class="event" id="creationDeCompte">creer un compte</div>
+        <div class="event" id="connexion">se connecter</div>
         <div class="event" id="vente">vendre</div>
         <div class="event" id="produits">consulter les articles</div>
         <div class="event" id="utilisateurs">consulter la liste des utilisateurs</div>
         <script>
             var role = sessionStorage.getItem('Role');
             if(typeof role !== 'undefined'){
-              document.getElementsByClassName("connecteur").style.display = "none";
-
+              document.getElementsByClassName("event").style.display = "none";
             }
         </script>
         <script src ="../Javascript/redirect.js"></script>
