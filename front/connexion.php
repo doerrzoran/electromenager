ca7c6ec315242426529402b4e76b1f6991d6568b
zozo222 <doerrzoran@gmail.com>
2022-10-24 14:07:21 +0200

    amelioration formulaires

--- front/connexion.php
+++ front/connexion.php
@@ -10,9 +10,9 @@
         <div><?php require_once "header.html"; ?></div>
         <form id="contactForm" action="../back/scriptConnexion.php" method="POST" enctype="multipart/form-data">
             <label for="mail">adresse email</label>
-            <input id="email" type="text" name="mail">
+            <input id="email" type="text" name="mail" pattern=".+@g+.com" required>
             <label for="password">mot de passe</label>
-            <input type="text" name="password">
+            <input type="password" name="password" required>
             <button type="submit" id="event">vous identifier</button>
         </form>
     </body>
