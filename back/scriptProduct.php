bf4e156b9be0ed67d8d50091bd6193e851164feb
zozo222 <doerrzoran@gmail.com>
2022-10-27 12:02:07 +0200

    re

--- back/scriptProduct.php
+++ back/scriptProduct.php
@@ -2,18 +2,18 @@
 <?php
 require_once 'dbbFunctions.php';
 echo 'test</br>';
-// var_dump($_POST);
+var_dump($_POST);
 
-// if($_POST){
-//     $infoProduct = $_POST;
-//     $productType = $infoProduct["type"]
-//     $productLabel = $infoProduct["Label"]
-//     $productPicture = $infoProduct["picture"]
-//     $productDescription = $infoProduct["description"]
-//     $productPrice = $infoProduct["price"]
+if($_POST){
+    $infoProduct = $_POST;
+    $productType = $infoProduct["type"]
+    $productLabel = $infoProduct["Label"]
+    $productPicture = $infoProduct["picture"]
+    $productDescription = $infoProduct["description"]
+    $productPrice = $infoProduct["price"]
 
 
     
-//     // $product = insertIntoDatabase("_product", "type, Label, picture, description, price", "'$productType', '$productLabel', '$productPicture', '$productDescription', '$productPrice'", $conn);
+    // $product = insertIntoDatabase("_product", "type, Label, picture, description, price", "'$productType', '$productLabel', '$productPicture', '$productDescription', '$productPrice'", $conn);
 
-// }
+}
