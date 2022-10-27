4cabc63b9003f10edbd8f4584e8c76f1cc2647ae
zozo222 <doerrzoran@gmail.com>
2022-10-27 11:27:19 +0200

    re

--- back/scriptProduct.php
+++ back/scriptProduct.php
@@ -1,18 +1,19 @@
+<!DOCTYPE html>
 <?php
-require_once "dbbFunction.php";
+// require_once "dbbFunction.php";
 echo 'test</br>';
-var_dump($_POST);
+// var_dump($_POST);
 
-if($_POST){
-    $infoProduct = $_POST;
-    $productType = $infoProduct["type"]
-    $productLabel = $infoProduct["Label"]
-    $productPicture = $infoProduct["picture"]
-    $productDescription = $infoProduct["description"]
-    $productPrice = $infoProduct["price"]
+// if($_POST){
+//     $infoProduct = $_POST;
+//     $productType = $infoProduct["type"]
+//     $productLabel = $infoProduct["Label"]
+//     $productPicture = $infoProduct["picture"]
+//     $productDescription = $infoProduct["description"]
+//     $productPrice = $infoProduct["price"]
 
 
     
-    // $product = insertIntoDatabase("_product", "type, Label, picture, description, price", "'$productType', '$productLabel', '$productPicture', '$productDescription', '$productPrice'", $conn);
+//     // $product = insertIntoDatabase("_product", "type, Label, picture, description, price", "'$productType', '$productLabel', '$productPicture', '$productDescription', '$productPrice'", $conn);
 
-}
+// }
