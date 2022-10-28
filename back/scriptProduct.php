020c340840c3a42b1e851b1f3102db43cb184d17
zozo222 <doerrzoran@gmail.com>
2022-10-28 13:53:03 +0200

    re

--- back/scriptProduct.php
+++ back/scriptProduct.php
@@ -11,9 +11,9 @@ $productLabel = $_POST["Label"];
 $productDescription = $_POST["description"];
 $productPrice = $_POST["price"];
 
-$target_dir = "images/";
+$target_dir = "../images/";
 $target_file = $target_dir . basename($_FILES["image"]["name"]);
-move_uploaded_file($_FILES["image"]["tmp_name"], "../".$target_file);
+move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
 
 
 
