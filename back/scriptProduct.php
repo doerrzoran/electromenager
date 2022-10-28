f4269d606392857bf1cb17662686d91857ae95b8
zozo222 <doerrzoran@gmail.com>
2022-10-28 15:03:37 +0200

    re

--- back/scriptProduct.php
+++ back/scriptProduct.php
@@ -5,15 +5,19 @@ var_dump($_POST);
 var_dump($_FILES);
 
 
-// $productSeller = $_POST["seller"];
-// $productType = $_POST["type"];
-// $productLabel = $_POST["Label"];
-// $productDescription = $_POST["description"];
-// $productPrice = $_POST["price"];
+$productSeller = $_POST["seller"];
+$productType = $_POST["type"];
+$productLabel = $_POST["Label"];
+$productDescription = $_POST["description"];
+$productPrice = $_POST["price"];
 
 $target_dir = "../images/";
 $target_file = $target_dir . basename($_FILES["image"]["name"]);
-copy($_FILES["image"]["tmp_name"], $target_file);
+if(move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)){
+    echo 'upload reussi !';
+}else{
+    echo 'echec';
+}
 
 
 
