b0fc748f54380546934037b5d22e439252f4ce0c
zozo222 <doerrzoran@gmail.com>
2022-10-28 13:25:54 +0200

    re

--- back/scriptProduct.php
+++ back/scriptProduct.php
@@ -1,26 +1,25 @@
 
 <?php
 require_once 'dbbFunctions.php';
+var_dump($_POST);
 var_dump($_FILES);
 
 
 $productSeller = $_POST["seller"];
 $productType = $_POST["type"];
 $productLabel = $_POST["Label"];
-$productPicture = $_POST["image"];
 $productDescription = $_POST["description"];
 $productPrice = $_POST["price"];
 
-var_dump($productPicture);
+$target_dir = "../mages/";
+$target_file = $target_dir . basename($_FILES["image"]["name"]);
+move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
+
+
 
 
     
-$product = insertIntoDatabase("_product", "seller, type, Label, picture, description, price", "'$productSeller', '$productType', '$productLabel', '$productPicture', '$productDescription', '$productPrice'", $conn);
+// $product = insertIntoDatabase("_product", "seller, type, Label, picture, description, price", "'$productSeller', '$productType', '$productLabel', '$productPicture', '$productDescription', '$productPrice'", $conn);
         //    insertIntoDatabase('User_acount', 'name, firstname, mail, password, role', "'$name', '$firstName', '$mail', '$password', 2", $conn);
 
-if($product != NULL){
-    echo '2';
-}else{
-    echo '1';
-};
 
