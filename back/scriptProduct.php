a80caad44dedc266098ad9a05d12a14e0cfc4ea9
zozo222 <doerrzoran@gmail.com>
2022-10-28 13:35:42 +0200

    re

--- back/scriptProduct.php
+++ back/scriptProduct.php
@@ -11,7 +11,7 @@ $productLabel = $_POST["Label"];
 $productDescription = $_POST["description"];
 $productPrice = $_POST["price"];
 
-$target_dir = "images/";
+$target_dir = "electromenager/images/";
 $target_file = $target_dir . basename($_FILES["image"]["name"]);
 move_uploaded_file($_FILES["image"]["tmp_name"], $target_file);
 
