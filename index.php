e575dacb3a65fb9bc1eac4f6629f20eb244cd750
zozo222 <doerrzoran@gmail.com>
2022-11-02 11:11:42 +0100

    installation Simple Files

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <html>
+<script src="https://app.simplefileupload.com/buckets/b8bc1f8a3fed9fd752a08588100a7e0b.js"></script>
     <body>
         <div>electromenager</div>
         <?php header('Location: front/pageAccueil.php'); ?>
