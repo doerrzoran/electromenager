bbbd7733d26b2092a4ac2815cbe6fdf9f6d8f5b9
zozo222 <doerrzoran@gmail.com>
2022-11-02 12:04:50 +0100

    re

--- front/pageAccueil.php
+++ front/pageAccueil.php
@@ -1,5 +1,9 @@
 <html>
+<head>
+    <script src="https://app.simplefileupload.com/buckets/b8bc1f8a3fed9fd752a08588100a7e0b.js"></script>
+</head>
     <body>
+    <input type="hidden" name="images" id="images" class="simple-file-upload" value="C:\MAMP\htdocs\electromenager\pictures\test.txt">
         <div>
             <?php
             require_once "header.html";
