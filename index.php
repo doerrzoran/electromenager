6d83080e49a2f456c41d64895a72f395ebe33a50
zozo222 <doerrzoran@gmail.com>
2022-11-02 11:25:26 +0100

    re

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <script src="https://app.simplefileupload.com/buckets/b8bc1f8a3fed9fd752a08588100a7e0b.js"></script>
-<input type="hidden" name="images" id="images" class="simple-file-upload" value="pictures\photoAspirateur.jpg">
+<input type="hidden" name="images" id="images" class="simple-file-upload" value="C:\MAMP\htdocs\electromenager\pictures\photoAspirateur.jpg">
     <body>
         <div>electromenager</div>
         <?php header('Location: front/pageAccueil.php'); ?>
