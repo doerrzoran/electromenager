1e5804763136ff105b1682a1992a5010ea5340dd
zozo222 <doerrzoran@gmail.com>
2022-11-04 16:31:21 +0100

    re

--- back/displayProduct.php
+++ back/displayProduct.php
@@ -3,15 +3,13 @@
 require_once "../back/dbbFunctions.php";
 
 $products = selectAllFromDatabase("_product", $conn);
-var_dump($products);
-foreach($products as $product){
-    print_r($product['label']);
-}
+
+
 ?>
 
-<!-- <script>
+<script>
 var arr = <?php echo json_encode($products); ?>;
 </script>
 <script src="../Javascript/redirectUsersInfo.js"></script>
-<script> window.location.href = "../front/listeProduits.php" </script> -->
+<script> window.location.href = "../front/listeProduits.php" </script>
 
