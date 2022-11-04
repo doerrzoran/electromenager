6aeccb3ba4d399b974d9f0a9c88f1c767098f41a
zozo222 <doerrzoran@gmail.com>
2022-11-04 12:06:13 +0100

    Affichage des produits

--- back/displayProduct.php
+++ back/displayProduct.php
@@ -4,7 +4,12 @@ require_once "../back/dbbFunctions.php";
 
 $products = selectAllFromDatabase("_product", $conn);
 
-foreach($products as $product){
-    var_dump($product);
-}
+
+?>
+
+<script>
+var arr = <?php echo json_encode($products); ?>;
+</script>
+<script src="../Javascript/redirectUsersInfo.js"></script>
+<script> window.location.href = "../front/listeProduits.php" </script>
 
--- front/listeProduits.php
+++ front/listeProduits.php
@@ -1,6 +1,24 @@
 <?php 
 require_once "header.html"; 
-require_once "../back/displayProduct.php"; 
 ?>
 
-<script src ="../Javascript/redirect.js"></script>
+<html>
+  <body>
+    <script>
+    var data = sessionStorage.getItem('Cle');
+    var newArr = JSON.parse(data);
+    function tab(newArr){
+      for(let i = 0; i < newArr.length; i++){
+        var arr = newArr[i];
+        document.write(arr + '<br>');
+      };
+    }
+    </script>
+    <?php require_once "header.html";  
+    $variable = "<script>tab(newArr);</script>";
+    echo $variable;
+    ?>
+  </body>
+  
+  <script src ="../Javascript/redirect.js"></script>
+</html>
