4cd36e5aa82f91de7c9108cc9d082ebd91fc7863
zozo222 <doerrzoran@gmail.com>
2022-11-04 17:12:28 +0100

    re

--- front/listeProduits.php
+++ front/listeProduits.php
@@ -10,6 +10,8 @@ require_once "header.html";
     function tab(newArr){
       for(let i = 0; i < newArr.length; i++){
         var arr = newArr[i];
+        var image = arr['image'];
+        var picture = '<img src="'+ image +'" >';
         document.write(arr['label'] + arr['seller'] + arr['price'] + '<br>');
       };
     }
