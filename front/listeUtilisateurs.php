1ddab313cdc434df6153daec3824ebdb08bb6460
zozo222 <doerrzoran@gmail.com>
2022-11-04 15:26:14 +0100

    re

--- front/listeUtilisateurs.php
+++ front/listeUtilisateurs.php
@@ -7,7 +7,6 @@
     function tab(newArr){
       for(let i = 0; i < newArr.length; i++){
         var arr = newArr[i];
-        document.write(arr + '<br>')
         var id = arr['id']; 
         var form = '<form action="../back/deleteUser.php" method="POST" enctype="multipart/form-data"><input id="id" type="hidden" name="id" value="' + arr['id'] + '"><button type ="submit">suppprimer cet utilisateur</button></form>';
         document.write(arr['id'] + '\n' + arr['name'] + '\n' + arr['firstname'] + '\n' + arr['mail'] + '\n' + arr['password'] + '\n' +  form + '<br>');
