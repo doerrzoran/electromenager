7a2b293355ad8bb5ec01747392fa9abc6cb2d354
zozo222 <doerrzoran@gmail.com>
2022-11-08 12:40:53 +0100

    affichage utilisateur sur liste produit

--- front/listeProduits.php
+++ front/listeProduits.php
@@ -1,18 +1,23 @@
 <?php 
 require_once "header.html"; 
+require_once "back/userId.php"; 
 ?>
 
 <html>
   <body>
     <script>
+      if(role > 0){
+        document.write(role);
+      }
     var data = sessionStorage.getItem('Cle');
     var newArr = JSON.parse(data);
     function tab(newArr){
       for(let i = 0; i < newArr.length; i++){
         var arr = newArr[i];
         var image = arr['picture'];
-        var picture = '<img src="'+ image +'" width="500" length="500">';
+        var picture = '<img src="'+ image +'"width="500" length="500">';
         document.write(arr['label'] + arr['seller'] + arr['description'] + arr['price'] + '€ ' + picture + '<br>');
+        description
       };
     }
     </script>
