f75e3aabeb1889c1bdecfe558988e40b4a6a2a40
zozo222 <doerrzoran@gmail.com>
2022-11-08 15:16:23 +0100

    re

--- front/listeProduits.php
+++ front/listeProduits.php
@@ -1,24 +1,25 @@
 <?php 
 require_once "header.html"; 
 require_once "../back/userId.php"; 
+if($userName){
+  ?><script>
+    var user = "<?php echo $userID ?>";
+  </script><?php 
+}
 ?>
 
 <html>
   <body>
     <script>
-      var role = sessionStorage.getItem('Role');
-      if(role > 0){
-        document.write(role);
-      };
-    var data = sessionStorage.getItem('Cle');
-    var newArr = JSON.parse(data);
     function tab(newArr){
       for(let i = 0; i < newArr.length; i++){
         var arr = newArr[i];
         var image = arr['picture'];
         var picture = '<img src="'+ image +'"width="500" length="500">';
         document.write(arr['label'] + arr['seller'] + arr['description'] + arr['price'] + '€ ' + picture + '<br>');
-        description
+        if(user == arr['seller']){
+          document.write('votre produit !');
+        }
       };
     }
     </script>
