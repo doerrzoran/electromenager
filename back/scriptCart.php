87a2a848075691a127900519368e527124d60f10
zozo222 <doerrzoran@gmail.com>
2022-11-21 17:18:29 +0100

    test redirection

--- back/scriptCart.php
+++ back/scriptCart.php
@@ -1,8 +1,19 @@
+
 <?php
 require_once 'dbbFunctions.php';
 require_once 'userId.php';
 
+?>
+<script>
+user = <?php echo $userID ?>
+if(user = null){
+  window.location.href = "../front/creationDeCompte.php";
+}
+</script>
+
+<?php
 $user = $userID;
 $product = $_POST["product"];
 
 $product = insertIntoDatabase("_cart", "client, product", "'$user', '$product'", $conn);
+
