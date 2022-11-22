22595991f965da6438b88df32c77d6287448172e
zozo222 <doerrzoran@gmail.com>
2022-11-22 14:40:28 +0100

    re

--- back/displayCart.php
+++ back/displayCart.php
@@ -12,4 +12,10 @@ foreach($carts as $cart){
 }
 
 var_dump($products);
+?>
 
+<script>
+var arr = <?php echo json_encode($products); ?>;
+</script>
+<script src="../Javascript/redirectUsersInfo.js"></script>
+<script> window.location.href = "../front/panier.php" </script>
