e6cce57f529b5959c97f54f15e174839442fb2eb
zozo222 <doerrzoran@gmail.com>
2022-11-22 11:47:40 +0100

    re

--- back/displayCart.php
+++ back/displayCart.php
@@ -3,8 +3,12 @@
 require_once "../back/dbbFunctions.php";
 require_once "../back/userId.php";
 
-$cart = selectMultipleFromDatabase("_cart", "client", $userID, $conn);
-print_r($cart);
-// $products = selectMultipleFromDatabase("_product", "id", , $conn);
+$carts = selectMultipleFromDatabase("_cart", "client", $userID, $conn);
+print_r($carts);
+foreach($carts as $cart){
+ $productID = $cart['product'];
+ echo $productID;
+}
+// $product = selectFromDatabase("_product", "id", "", $conn);
 // ?>
 
