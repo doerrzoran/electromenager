ba35d00655f8ef53a5686a63e987fda84c260bf4
zozo222 <doerrzoran@gmail.com>
2022-11-22 15:29:55 +0100

    re

--- front/panier.php
+++ front/panier.php
@@ -14,6 +14,7 @@ welcomeUser($userName, $userFirstname);
       console.log(newArr);
       for(let i = 0; i < newArr.length; i++){
         var arr = newArr[i];
+        document.write(arr[1]);
         var image = arr['picture'];
         var picture = '<img src="'+ image +'"width="500" length="500">';
         document.write(arr['label'] + ' ' + arr['seller'] + ' ' + arr['description'] + ' ' + arr['price'] + '€ ' + ' ' + picture + '<br>');
