63bb97530719908d94bbdc08c3594760a6832013
zozo222 <doerrzoran@gmail.com>
2022-11-22 11:39:04 +0100

    re

--- back/displayCart.php
+++ back/displayCart.php
@@ -8,8 +8,3 @@ print_r($cart);
 // $products = selectMultipleFromDatabase("_product", "id", , $conn);
 // ?>
 
-// <script>
-// var arr = <?php echo json_encode($products); ?>;
-// </script>
-// <script src="../Javascript/redirectUsersInfo.js"></script>
-// <script> window.location.href = "../front/panier.php" </script>
