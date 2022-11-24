831560c125050173b7629511e5c61e5e53833a5d
zozo222 <doerrzoran@gmail.com>
2022-11-24 13:16:33 +0100

    re

--- back/deleteUser.php
+++ back/deleteUser.php
@@ -8,7 +8,7 @@ $product = selectFromDatabase("_product", "seller", $id, $conn);
 
 var_dump($product);
 print_r($product);
-echo $product[1];
+echo $product[0];
 
 // $deletionProduct = deleteFromDatabase("_product", "id", $product, $conn);
 
