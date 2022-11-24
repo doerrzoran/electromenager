467c101f8976ed51cbdd7f9876619151e7cf2990
zozo222 <doerrzoran@gmail.com>
2022-11-24 15:03:50 +0100

    impossibilité de supprimer l'administrateur

--- front/listeUtilisateurs.php
+++ front/listeUtilisateurs.php
@@ -7,9 +7,13 @@
     function tab(newArr){
       for(let i = 0; i < newArr.length; i++){
         var arr = newArr[i];
-        var id = arr['id']; 
         var form = '<form action="../back/deleteUser.php" method="POST" enctype="multipart/form-data"><input id="id" type="hidden" name="id" value="' + arr['id'] + '"><button type ="submit">suppprimer cet utilisateur</button></form>';
-        document.write(arr['id'] + '\n' + arr['name'] + '\n' + arr['firstname'] + '\n' + arr['mail'] + '\n' + arr['password'] + '\n' +  form + '<br>');
+        document.write(arr['id'] + '\n' + arr['name'] + '\n' + arr['firstname'] + '\n' + arr['mail'] + '\n' + arr['password'] + '\n' + '<br>');
+        if(arr['role'] == 1){
+          document.write('administrateur ');
+        }else{
+          document.write(form);
+        }
       };
     }
     </script>
