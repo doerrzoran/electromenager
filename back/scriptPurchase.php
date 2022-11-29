5517d9a19f9a03f24182d45cebb476e072b5f154
zozo222 <doerrzoran@gmail.com>
2022-11-29 11:09:26 +0100

    re

--- back/scriptPurchase.php
+++ back/scriptPurchase.php
@@ -1,12 +1,12 @@
 <?php
 require_once 'dbbFunctions.php';
-
+require_once 'userId.php';
 
 
 $user = $userID;
 $product = $_POST["product"];
 
     
-$product = insertIntoDatabase("_purchase", "product", "'$product'", $conn);
+$product = insertIntoDatabase("_purchase", "buyer, product", "'$user', '$product'", $conn);
 
 // header('Location: ../front/listeProduits.php');
