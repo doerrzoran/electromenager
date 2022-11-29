a795c06f66877a3dbe679316739d686dea244101
zozo222 <doerrzoran@gmail.com>
2022-11-29 13:39:13 +0100

    re

--- back/scriptPurchase.php
+++ back/scriptPurchase.php
@@ -4,10 +4,13 @@ require_once 'userId.php';
 
 
 $user = $userID;
+echo $user
 $product = $_POST["product"];
+echo $product;
 $date = date('d/m/yy');
+echo $date;
 
     
-$product = insertIntoDatabase("_purchase", "buyer, product, date", "'$user', '$product', current_timestamp", $conn);
+// $product = insertIntoDatabase("_purchase", "buyer, product, date", "'$user', '$product', current_timestamp", $conn);
 
 // header('Location: ../front/listeProduits.php');
