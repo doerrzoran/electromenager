ba34c8b5196b076cb2cbea60ddd2faa5a35a3fff
zozo222 <doerrzoran@gmail.com>
2022-11-30 16:04:09 +0100

    re

--- back/scriptPurchase.php
+++ back/scriptPurchase.php
@@ -12,6 +12,7 @@ $cart = selectFromDatabase("_cart", "client", $userID, $conn);
 
 $cartID = $cart[0];
 $deletionCart = deleteFromDatabase("_cart", "id", $cartID, $conn);
+echo $product;
 $deletionProduct = deleteFromDatabase("_product", "id", $product, $conn);
 
-header('Location: ../front/pageUtilisateur.php');
+// header('Location: ../front/pageUtilisateur.php');
