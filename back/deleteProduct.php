2a536a011cf9c876f2ad418716abfbf97274c64f
zozo222 <doerrzoran@gmail.com>
2022-12-01 14:56:48 +0100

    re

--- back/deleteProduct.php
+++ back/deleteProduct.php
@@ -4,6 +4,8 @@ require_once 'dbbFunctions.php';
 
 $product = $_POST['product'];
 
+$deletionPurchase = deleteFromDatabase("_purchase", "product", $product, $conn);
+
 $deletion = deleteFromDatabase("_product", "id", $product, $conn);
 
 header("Location: displayProduct.php");
