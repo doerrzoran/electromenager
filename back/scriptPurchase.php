464c75073619cff9ddb0648ca144a922aaa9ceb2
zozo222 <doerrzoran@gmail.com>
2022-12-01 12:23:57 +0100

    re

--- back/scriptPurchase.php
+++ back/scriptPurchase.php
@@ -11,7 +11,9 @@ $purchase = insertIntoDatabase("_purchase", "buyer, product, date", "'$user', '$
 $cart = selectFromDatabase("_cart", "client", $userID, $conn);
 
 $cartID = $cart[0];
-echo $product;
-$deletionProduct = deleteFromDatabase("_product", "id", $product, $conn);
 $deletionCart = deleteFromDatabase("_cart", "id", $cartID, $conn);
+echo $product;
+
+$deletionProduct = deleteFromDatabase("_product", "id", $product, $conn);
+
 // header('Location: ../front/pageUtilisateur.php');
