e40b505a0b18060a401df524c405b4b603bd3668
zozo222 <doerrzoran@gmail.com>
2022-12-01 14:45:01 +0100

    re

--- back/deleteUser.php
+++ back/deleteUser.php
@@ -9,6 +9,9 @@ $products = selectFromDatabase("_product", "seller", $id, $conn);
 $product = $products[0];
 
 $deletionUser = deleteFromDatabase("user_acount", "id", $id, $conn);
+$cart = selectFromDatabase("_cart", "client", $id, $conn);
+$cartID = $cart[0];
+$deletionCart = deleteFromDatabase("_cart", "id", $cartID, $conn);
 
 $deletionProduct = deleteFromDatabase("_product", "seller", $id, $conn);
 
