a718a62aa7a6c9c81c2f5b98b1a61010053de356
zozo222 <doerrzoran@gmail.com>
2022-12-02 15:01:08 +0100

    re

---
+++ front/historiqueDesAchats.php
@@ -0,0 +1,26 @@
+<?php 
+require_once "header.html"; 
+require_once "welcomeUser.php";
+welcomeUser($userName, $userFirstname);
+
+?>
+
+<html>
+  <body>
+    <script>
+     var data = sessionStorage.getItem('Cle');
+     var newArr = JSON.parse(data);
+    function tab(newArr){
+      for(let i = 0; i < newArr.length; i++){
+        document.write(arr[1] + ' ' + arr[2] + ' ' + arr[3] + '€ ' + ' ' + '<br>');
+      };
+    }
+    </script>
+    <?php  
+    $variable = "<script>tab(newArr);</script>";
+    echo $variable;
+    ?> 
+  </body>
+  
+  <script src ="../Javascript/redirect.js"></script>
+</html>
