ba58ac19bd4aa76fc2bb2e69d6d3a57a9e1ceb97
zozo222 <doerrzoran@gmail.com>
2022-12-02 13:25:23 +0100

    re

--- front/pageUtilisateur.php
+++ front/pageUtilisateur.php
@@ -8,6 +8,7 @@ welcomeUser($userName, $userFirstname);
 <html>
   <body>
     <div class="event" id ="cart">consulter votre panier</div>
+    <div class="event" id ="purchase">consulter vos achats</div>
     <script>
      var data = sessionStorage.getItem('Cle');
      var newArr = JSON.parse(data);
