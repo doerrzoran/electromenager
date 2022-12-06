10d01976778a969965a5f4868b7ae37f451ced5a
zozo222 <doerrzoran@gmail.com>
2022-12-06 09:29:03 +0100

    re

--- front/historiqueDesAchats.php
+++ front/historiqueDesAchats.php
@@ -14,7 +14,7 @@ welcomeUser($userName, $userFirstname);
     function tab(newArr){
       for(let i = 0; i < newArr.length; i++){
         arr = newArr[i];
-        document.write(arr[1] + ' ' + arr[2] + ' ' + arr['price'] + '€ ' + ' ' + '<br>');
+        document.write(arr[4DATE] + ' ' + arr['price'] + '€ ' + ' ' + '<br>');
       };
     }
     </script>
