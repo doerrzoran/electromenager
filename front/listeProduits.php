155b125a37621934a8986cbb2fc2770ae69d4dff
zozo222 <doerrzoran@gmail.com>
2022-12-06 16:43:40 +0100

    re

--- front/listeProduits.php
+++ front/listeProduits.php
@@ -28,8 +28,9 @@ if($userID){
         var image = arr['picture'];
         var picture = '<img class="img-fluid img-thumbnail" alt="Responsive image" src="'+ image +'"width="500" length="500">';
         var form = '<form action="../back/deleteProduct.php" method="POST" enctype="multipart/form-data"><input id="id" type="hidden" name="product" value="' + arr['id'] + '"><button type ="submit">suppprimer ce produit</button></form>';
-        document.write(arr['label'] + ' ' + arr['seller'] + ' ' + arr['description'] + ' ' + arr['price'] + '€ ' + '<br>');
+        document.write(arr['label'] + ' ' + '<br>');
         document.write(picture);
+        document.write(arr['description'] + ' ' + arr['price'] + '€ ' + '<br>');
         var purchase = '<form action="../back/scriptCart.php" method="POST" enctype="multipart/form-data"><input id="id" type="hidden" name="product" value="' + arr['id'] + '"><button type ="submit">ajouter ce produit à votre panier</button></form>';
         if(user == arr['seller']){
           document.write('votre article ');
