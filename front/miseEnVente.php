434609d6bd7bc8ebfc22d7bc097b2b9c86544942
zozo222 <doerrzoran@gmail.com>
2022-12-20 14:28:47 +0100

    re

--- front/miseEnVente.php
+++ front/miseEnVente.php
@@ -16,8 +16,10 @@
     
 
     <form action="../back/scriptProduct.php" method="POST" enctype="multipart/form-data">
-        <label for="type">type:</label>
-        <select name="type">
+        <input type="hidden" name="seller" value="<?php echo $userID ?>">
+        <div>
+          <label for="type">type:</label>
+          <select name="type">
             <option value="1">micro onde</option>
             <option value="2">four</option>
             <option value="3">mixeur</option>
@@ -26,16 +28,28 @@
             <option value="6">fer à repasser</option>
             <option value="7">fouet</option>
             <option value="8">aspirateur</option>
-        </select>
-        <input type="hidden" name="seller" value="<?php echo $userID ?>">
-        <label for="Label">Label:</label>
-        <input type="text" name="Label" required>
+          </select>
+        </div>
+        <div>
+          <label for="Label">Label:</label>
+          <input type="text" name="Label" required>
+        </div>
+        <div> 
+          <label for="description">description :</label>
+          <textarea name="description" id="" cols="30" rows="10" minlength="20" maxlength="30" required >description</textarea>
+        </div>
+        <div>
+          <label for="price">prix:</label>
+          <input type="number" name="price" required>  
+        </div>
+        
+        
+        
+        
         <input type="hidden" id="image" class="simple-file-upload" required>
-        <input type="hidden" name="image" id="picture" required>
-        <label for="description">description :</label>
-        <textarea name="description" id="" cols="30" rows="10" minlength="20" maxlength="30" required >description</textarea>
-        <label for="price">prix:</label>
-        <input type="number" name="price" required>  
+        <input type="text" name="image" id="picture" required>
+       
+        
         <button type="submit">enregistrer</button>
     </form>
  </body>
