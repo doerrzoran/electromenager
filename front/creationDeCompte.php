bba3d6cd0deec9b7c51ca6ca93abac18dd489458
zozo222 <doerrzoran@gmail.com>
2022-12-20 12:35:37 +0100

    re

--- front/creationDeCompte.php
+++ front/creationDeCompte.php
@@ -10,17 +10,19 @@
         require_once "header.html"; 
         ?>
         <form action="../back/scriptCreationDeCompte.php" method="POST" enctype="multipart/form-data">
-            <label for="nom" >nom:</label>
-            <input type="text" name="nom" required>
-            <label for="prenom">prenom:</label>
-            <input type="text" name="prenom" required>
-            <label for="mail">addresse mail</label>
-            <input type="email" name="mail" pattern="+@g+.com" required>
-            <label for="Password">mot de passe</label>
-            <input type="password" name="password" minlength="5" maxlength="10" required >
-            <label for="password2">confirmer le mot de pase</label>
-            <input type="password" name="password2" minlength="5" maxlength="10" required>
-            <input type="submit"> </input>
+            <div class="form-group">
+                <label for="nom" >nom:</label>
+                <input type="text" name="nom" required>
+                <label for="prenom">prenom:</label>
+                <input type="text" name="prenom" required>
+                <label for="mail">addresse mail</label>
+                <input type="email" name="mail" pattern="+@g+.com" required>
+                <label for="Password">mot de passe</label>
+                <input type="password" name="password" minlength="5" maxlength="10" required >
+                <label for="password2">confirmer le mot de pase</label>
+                <input type="password" name="password2" minlength="5" maxlength="10" required>
+                <input type="submit"> </input>
+            </div>
         </form>
     </body>
     <?php
