02a6a1d70b6725d231bffd92658ce30074682168
zozo222 <doerrzoran@gmail.com>
2022-12-20 15:52:21 +0100

    re

--- front/header.php
+++ front/header.php
@@ -7,15 +7,17 @@
     </head>
     <body class="bg-warning" style="padding: 30px;">
       <div class="page-header">
-        <header class="p-3 mb-2 bg-info text-white">
-           <div class="text-danger" style = 'padding-bottom: 20px;'>
+        <header class="p-3 mb-2 bg-info text-white" style="padding-bottom: 30px;">
+           <div class="text-danger" style = ''>
              <h1 class="event" id="title" style="cursor:pointer; font-size: 45px;">Electromenager</h1>
-        </header>
-        <?php
-             require_once "welcomeUser.php";
-             welcomeUser($userName, $userFirstname); 
-             ?>
            </div>
+        </header>
+        <div class="text-danger" style = ''>
+        <?php
+          require_once "welcomeUser.php";
+          welcomeUser($userName, $userFirstname); 
+          ?>
+        </div>
        </div>
         
 
