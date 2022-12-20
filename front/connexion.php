9fff08b0e77347e1b93d83be73de9bfb230fc8db
zozo222 <doerrzoran@gmail.com>
2022-12-20 14:17:55 +0100

    re

--- front/connexion.php
+++ front/connexion.php
@@ -11,10 +11,14 @@
     <body>
         <div><?php require_once "header.html"; ?></div>
         <form id="contactForm" action="../back/scriptConnexion.php" method="POST" enctype="multipart/form-data">
+          <div class="form-group">
             <label for="mail">adresse email</label>
             <input id="email" type="text" name="mail" pattern="+@+.com" required>
+          </div>
+          <div class="form-group">
             <label for="password">mot de passe</label>
-            <input type="password" name="password" minlength="5" maxlength="13" required>
+            <input type="password" name="password" minlength="5" maxlength="13" required>  
+          </div>
             <button type="submit" id="event">vous identifier</button>
         </form>
     </body>
