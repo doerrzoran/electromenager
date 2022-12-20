174562b0827f237722f39ba5d922ccda711f1fe4
zozo222 <doerrzoran@gmail.com>
2022-12-20 14:40:11 +0100

    re

--- front/miseEnVente.php
+++ front/miseEnVente.php
@@ -35,7 +35,7 @@
           <input type="text" name="Label" required>
         </div>
         <div> 
-          <label for="description">description :</label>
+          <label for="description"></label>
           <textarea name="description" id="" cols="50" rows="3" minlength="20" maxlength="30" required >description</textarea>
         </div>
         <div>
