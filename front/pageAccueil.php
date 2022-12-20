5d1ee6f283e30013466fd72853991a6fc66535c2
zozo222 <doerrzoran@gmail.com>
2022-12-20 15:55:59 +0100

    alignement connection

--- front/pageAccueil.php
+++ front/pageAccueil.php
@@ -5,7 +5,7 @@
 ?>
 
         <div class="text-primary">
-            <div class="event" id="creationDeCompte" style="cursor:pointer; font-size: 20px; padding-bottom: 10px;">creer un compte</div>
+            <div class="event" id="creationDeCompte" style="cursor:pointer; font-size: 20px; padding-bottom: 10px; text-align: left">creer un compte</div>
             <div class="event" id="connexion" style="cursor:pointer; font-size: 20px; padding-bottom: 10px;">se connecter</div>
             <div class="event" id="utilisateur" style="cursor:pointer; font-size: 20px; padding-bottom: 10px;">consulter votre page</div>
             <div class="event" id="vente" style="cursor:pointer; font-size: 20px; padding-bottom: 10px;">vendre</div>
