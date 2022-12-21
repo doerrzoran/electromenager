a1f393b981ef044b4652e446d6086599cbb34e54
zozo222 <doerrzoran@gmail.com>
2022-12-21 15:26:03 +0100

    mise en page image

--- front/pageAccueil.php
+++ front/pageAccueil.php
@@ -8,7 +8,7 @@
           <div class="event" id="connexion" style="cursor:pointer; font-size: 20px; padding-bottom: 10px;">se connecter</div>
         </div>
         <div class="text-primary">
-          <div class="event" id="utilisateur" style="cursor:pointer; font-size: 20px; padding-bottom: 10px;">consulter votre page</div>
+          <div class="event" id="utilisateur"  style="cursor:pointer; font-size: 20px; padding-bottom: 10px;">consulter votre page</div>
           <div class="event" id="vente" style="cursor:pointer; font-size: 20px; padding-bottom: 10px;">vendre</div>
           <div class="event" id="produits" style="cursor:pointer; font-size: 20px; padding-bottom: 10px;">consulter les articles</div>
           <div class="event" id="utilisateurs" style="cursor:pointer; font-size: 20px; padding-bottom: 10px;">consulter la liste des utilisateurs</div>
