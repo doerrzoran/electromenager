80f86ddc388d1834c34eba8ac93d66b2bccd595c
zozo222 <doerrzoran@gmail.com>
2022-12-21 16:17:53 +0100

    re

--- front/listeProduits.php
+++ front/listeProduits.php
@@ -26,10 +26,9 @@ if($userID){
       for(let i = 0; i < newArr.length; i++){
         var arr = newArr[i];
         var image = arr['picture'];
-        var picture = '<table><tr><td><img style="vertical-align: top;" class="text-justify figure-img  img-fluid img-thumbnail" alt="Responsive image" src="'+ image +'"width="300" length="300" style="display: inline-block"></td>';
+        var picture = '<img style="vertical-align: top;" class="text-justify figure-img  img-fluid img-thumbnail" alt="Responsive image" src="'+ image +'"width="300" length="300" style="display: inline-block">';
         var form = '<form action="../back/deleteProduct.php" method="POST" enctype="multipart/form-data"><input id="id" type="hidden" name="product" value="' + arr['id'] + '"><button type ="submit">suppprimer ce produit</button></form>';
-        document.write(picture);
-        document.write('<td><p>' + arr['label'] + ' ' + '</p>' + '<p>' + arr['description'] + ' ' + '</p>' + '<p>' + arr['price'] + '€ ' + '</p></td></tr></table>');
+        document.write('<table><tr><th>' + arr['label'] + ' ' +'</th></tr><tr><td>' + picture + '</td>' + '<td><p>' + '</p>' + '<p>' + arr['description'] + ' ' + '</p>' + '<p>' + arr['price'] + '€ ' + '</p></td></tr></table>');
         var purchase = '<form action="../back/scriptCart.php" method="POST" enctype="multipart/form-data"><input id="id" type="hidden" name="product" value="' + arr['id'] + '"><button type ="submit">ajouter ce produit à votre panier</button></form>';
         if(user == arr['seller']){
           document.write('votre article ');
@@ -49,7 +48,8 @@ if($userID){
   <?php
      require_once "footer.html"; 
   ?>
-  
+  <th></th>
       
   <script src ="../Javascript/redirect.js"></script>
 </html>
+ 
