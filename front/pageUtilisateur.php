46210669f678fe8c89b8144128fe4a1c810ba8e7
zozo222 <doerrzoran@gmail.com>
2023-01-01 13:36:27 +0100

    re

--- front/pageUtilisateur.php
+++ front/pageUtilisateur.php
@@ -16,7 +16,7 @@ require_once "welcomeUser.php";
         var arr = newArr[i];
         var image = arr['picture'];
         var picture = '<img style="vertical-align: top;" class="text-justify figure-img  img-fluid img-thumbnail" alt="Responsive image" src="'+ image +'"width="300" length="300" style="display: inline-block">';
-        document.write('<table><tr><th>' + arr['label'] + ' ' +'</th></tr><tr><td>' + picture + '</td>' + '<td><p>' + '</p>' + '<p>' + arr['description'] + ' ' + '</p>' + '<p>' + arr['price'] + '€ ' + '</p></td></tr></table>');
+        document.write('<table style="padding-top: 50px"><tr><th>' + arr['label'] + ' ' +'</th></tr><tr><td>' + picture + '</td>' + '<td><p>' + '</p>' + '<p>' + arr['description'] + ' ' + '</p>' + '<p>' + arr['price'] + '€ ' + '</p></td></tr></table>');
         var form = '<form action="../back/deleteProduct.php" method="POST" enctype="multipart/form-data"><input id="id" type="hidden" name="product" value="' + arr['id'] + '"><button type ="submit">suppprimer ce produit</button></form>';
         document.write(form);
       };
