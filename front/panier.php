e89bb130a72cddc1557fa291953cda968fe380e0
zozo222 <doerrzoran@gmail.com>
2023-01-01 14:00:14 +0100

    re

--- front/panier.php
+++ front/panier.php
@@ -11,7 +11,7 @@ require_once "header.php";
       for(let i = 0; i < newArr.length; i++){
         var arr = newArr[i];
         var image = arr[4];
-        var picture = '<img src="'+ image +'"width="500" length="500">';
+        var picture = '<img style="vertical-align: top;" class="text-justify figure-img  img-fluid img-thumbnail" alt="Responsive image" src="'+ image +'"width="300" length="300" style="display: inline-block">';
         document.write('<div style="padding-top: 50px;"><table><tr><th>' + arr[3] + ' ' +'</th></tr><tr><td>' + picture + '</td>' + '<td><p>' + '</p>' + '<p>' + arr[5] + ' ' + '</p>' + '<p>' + arr[6] + '€ ' + '</p></td></tr></table></div>');
         var form = '<form action="../back/deleteCart.php" method="POST" enctype="multipart/form-data"><input id="id" type="hidden" name="product" value="' + arr[0] + '"><button type ="submit">retirer ce produit de votre panier</button></form>';
         var buy = '<form action="../back/scriptPurchase.php" method="POST" enctype="multipart/form-data"><input id="id" type="hidden" name="product" value="' + arr[0] + '"><button type ="submit">acheter</button></form>';
