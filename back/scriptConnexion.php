70ba6d445fc3a98c000c4acd181960688ef412e9
zozo222 <doerrzoran@gmail.com>
2023-01-09 10:56:28 +0100

    re

--- back/scriptConnexion.php
+++ back/scriptConnexion.php
@@ -13,9 +13,7 @@ $password = $infoConnexion['password'];
   ?>
   <script>
     var mail = sessionStorage.getItem('mail');
-    console.log(mail);
     var password = sessionStorage.getItem('password');
-    console.log(password);
     document.cookie = 'mail = ' + mail;
     document.cookie = 'password = ' + password;
     sessionStorage.removeItem('mail');
@@ -23,23 +21,21 @@ $password = $infoConnexion['password'];
   </script>
   <?php
   $mail = $_COOKIE['mail'];
-  echo $mail;
   $password = $_COOKIE['password'];
-  echo $password;
 }
 
 
-// $userMail = selectFromDatabase("User_acount", "mail", $mail, $conn);
-// $userMPassword = selectFromDatabase("User_acount", "password", $password, $conn);
+$userMail = selectFromDatabase("User_acount", "mail", $mail, $conn);
+$userMPassword = selectFromDatabase("User_acount", "password", $password, $conn);
 
 
-// if($userMail == false || $userMPassword == false){
-//   ?>
-//   <script>var message ='identifiants invalides';</script>
-//   <script src='../Javascript/redirectConnexion.js' ></script>
-//   <script>window.location.href = "../front/connexion.php";</script>
-//    <?php
-// }else{
-//   require 'userData.php';
-// }
+if($userMail == false || $userMPassword == false){
+  ?>
+  <script>var message ='identifiants invalides';</script>
+  <script src='../Javascript/redirectConnexion.js' ></script>
+  <script>window.location.href = "../front/connexion.php";</script>
+   <?php
+}else{
+  require 'userData.php';
+}
  
