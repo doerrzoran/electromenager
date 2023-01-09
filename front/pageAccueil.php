a063c6bac74fa42f4467e1230265a10a2ef75a34
zozo222 <doerrzoran@gmail.com>
2023-01-09 09:25:52 +0100

    re

--- front/pageAccueil.php
+++ front/pageAccueil.php
@@ -20,14 +20,18 @@
             if(role > 0){
               document.getElementById("creationDeCompte").style.display = "none";
               document.getElementById("connexion").style.display = "none";
-            }
-            if(role == 1){
+            }else{
                 document.getElementById("vente").style.display = "none";
                 document.getElementById("utilisateur").style.display = "none";
             }
             if(role != 1){
                 document.getElementById("utilisateurs").style.display = "none";
                 document.getElementById("administrateur").style.display = "none";
+                document.getElementById("utilisateur").style.display = "none";
+                document.getElementById("vente").style.display = "none";
+            }else{
+                document.getElementById("vente").style.display = "none";
+                document.getElementById("utilisateur").style.display = "none";
             }
         </script>
         <script src ="../Javascript/redirect.js"></script>
