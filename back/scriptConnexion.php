cc383517e63cf797befdcd32d79cb8f35147c355
zozo222 <doerrzoran@gmail.com>
2023-01-09 12:36:08 +0100

    re

--- back/scriptConnexion.php
+++ back/scriptConnexion.php
@@ -37,6 +37,15 @@ $password = $infoConnexion['password'];
   $password = $_COOKIE['password'];
   echo $password;
   
+  if (isset($_SERVER['HTTP_COOKIE'])) {
+    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+    foreach($cookies as $cookie) {
+        $parts = explode('=', $cookie);
+        $name = trim($parts[0]);
+        setcookie($name, '', time()-1000);
+        setcookie($name, '', time()-1000, '/');
+    }
+}
 }
 
 
