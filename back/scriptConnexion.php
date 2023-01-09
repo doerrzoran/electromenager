7848b3d31112c1a2c70d7e1f9fdcc1577f9508d9
zozo222 <doerrzoran@gmail.com>
2023-01-09 12:54:47 +0100

    re

--- back/scriptConnexion.php
+++ back/scriptConnexion.php
@@ -10,21 +10,9 @@ $mail = $infoConnexion['mail'];
 $password = $infoConnexion['password'];
 
 }else{
-  ?>
-  <script>
-    var mail = sessionStorage.getItem('mail');
-    console.log(mail);
-    var password = sessionStorage.getItem('password');
-    console.log(password);
-    document.cookie = 'mail = ' + mail;
-    document.cookie = 'password = ' + password;
-    sessionStorage.removeItem('mail');
-    sessionStorage.removeItem('password');
-  </script>
-  <?php
-  $mail = '<script>document.write(mail)</script>';
+  $mail = $_COOKIE['mail'];
   echo $mail;
-  $password = '<script>document.write(password)</script>';
+  $password = $_COOKIE['password'];
   echo $password;
 }
 
