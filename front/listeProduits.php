54108fc00737da9b027851d022fd4f01ffedde94
zozo222 <doerrzoran@gmail.com>
2023-01-09 16:29:04 +0100

    re

--- front/listeProduits.php
+++ front/listeProduits.php
@@ -26,18 +26,16 @@ if($userID){
         var arr = newArr[i];
         var image = arr['picture'];
         var picture = '<img style="vertical-align: top;" class="text-justify figure-img  img-fluid img-thumbnail" alt="Responsive image" src="'+ image +'"width="300" length="300" style="display: inline-block">';
-        var form = '<form action="../back/deleteProduct.php" method="POST" enctype="multipart/form-data"><input id="id" type="hidden" name="product" value="' + arr['id'] + '"><button type ="submit">suppprimer ce produit</button></form>';
         document.write('<div style="padding-top: 50px;"><table><tr><th>' + arr['label'] + ' ' +'</th></tr><tr><td>' + picture + '</td>' + '<td><p>' + '</p>' + '<p>' + arr['description'] + ' ' + '</p>' + '<p>' + arr['price'] + '€ ' + '</p></td></tr></table></div>');
         var purchase = '<form action="../back/scriptCart.php" method="POST" enctype="multipart/form-data"><input id="id" type="hidden" name="product" value="' + arr['id'] + '"><button type ="submit">ajouter ce produit à votre panier</button></form>';
         if(user == 0){
           document.write('Veuillez vous connecter pour effectuer un achat');
         }
-        if(user > 0){
+        if(user > 0 && user !== arr['seller']){
           document.write(purchase);
         }
         if(user == arr['seller']){
           document.write('votre article ');
-          document.write(form);
         }
       }
     }
