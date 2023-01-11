8f8b47a0e4c97bf9fad4df45327377330557284e
zozo222 <doerrzoran@gmail.com>
2023-01-11 10:10:43 +0100

    re

--- front/pageUtilisateur.php
+++ front/pageUtilisateur.php
@@ -3,7 +3,7 @@ require_once "header.php";
 require_once "welcomeUser.php";
 ?>
 <div class="text-primary" style="text-align: left; background-color: #FFFACD; border-radius: 15px; margin-right: 500px; font-size: 30px;">
-    <div class="event" id ="cart" style="cursor:pointer; font-size: 20px; padding-bottom: 10px;">consulter votre panier</div>
+    <div class="event" id ="cart" style="cursor:pointer; padding-bottom: 10px;">consulter votre panier</div>
     
     <script>
      var data = sessionStorage.getItem('Cle');
@@ -23,7 +23,7 @@ require_once "welcomeUser.php";
     $variable = "<script style='padding-top: 50px' >tab(newArr);</script>";
     echo $variable;
     ?>
-  <div class="event" id ="purchase" style="cursor:pointer; font-size: 20px; padding-bottom: 10px;">consulter vos achats</div>
+  <div class="event" id ="purchase" style="cursor:pointer; padding-top: 10px;">consulter vos achats</div>
 </div>
   <?php
     require_once "footer.html"; 
