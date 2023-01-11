e751ec15d64abdb4a96468a2d618e6f6683fedac
zozo222 <doerrzoran@gmail.com>
2023-01-11 11:18:00 +0100

    re

--- front/header.php
+++ front/header.php
@@ -5,7 +5,7 @@
         <link rel="stylesheet" type="text/css" href="../Assets/css/bootstrap.css">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
-    <body class="bg-warning container-fluid" style="background-size: 100; padding: 30px; background-image: url('https://cdn-g8hn2yqg.files-simplefileupload.com/static/blobs/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBMnQ1QVE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--651d33c2e055d45c70d059afa3d50502cb6d93fa/photo%20de%20pr%C3%A9sentation.jpg');">
+    <body class="bg-warning container-fluid" style="background-size: 100%; padding: 30px; background-image: url('https://cdn-g8hn2yqg.files-simplefileupload.com/static/blobs/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBMnQ1QVE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--651d33c2e055d45c70d059afa3d50502cb6d93fa/photo%20de%20pr%C3%A9sentation.jpg');">
         <header class="page-header p-3 mb-2 bg-info text-white" style="padding-bottom: 30px; border-radius: 15px;">
            <div class="text-danger" style = ''>
              <h1 class="event" id="title" style="cursor:pointer; font-size: 45px;">Electromenager</h1>
