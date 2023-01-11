c3df953494c8c595f2e6f5a85371ab31c48e0341
zozo222 <doerrzoran@gmail.com>
2023-01-11 09:36:05 +0100

    re

--- front/connexion.php
+++ front/connexion.php
@@ -10,17 +10,21 @@
     </script>
     <body>
         <div><?php require_once "header.php"; ?></div>
-        <form id="contactForm" action="../back/scriptConnexion.php" method="POST" enctype="multipart/form-data">
-          <div class="form-group">
-            <label for="mail">adresse email</label>
-            <input id="email" type="text" name="mail" pattern="+@+.com" required>
-          </div>
-          <div class="form-group">
-            <label for="password">mot de passe</label>
-            <input type="password" name="password" minlength="5" maxlength="13" required>  
-          </div>
-            <button type="submit" id="event">vous identifier</button>
-        </form>
+
+        <div class="text-primary" style="text-align: left; background-color: #FFFACD; border-radius: 15px; margin-right: 1300px;">
+          <form id="contactForm" action="../back/scriptConnexion.php" method="POST" enctype="multipart/form-data">
+            <div class="form-group">
+              <label for="mail">adresse email</label>
+              <input id="email" type="text" name="mail" pattern="+@+.com" required>
+            </div>
+            <div class="form-group">
+              <label for="password">mot de passe</label>
+              <input type="password" name="password" minlength="5" maxlength="13" required>  
+            </div>
+              <button type="submit" id="event">vous identifier</button>
+          </form>
+        </div>
+        
     </body>
     <?php
         require_once "footer.html"; 
