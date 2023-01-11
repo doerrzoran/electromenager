e8b58238822cf3ead9929a11b568eff18d28066b
zozo222 <doerrzoran@gmail.com>
2023-01-11 09:18:02 +0100

    re

--- front/welcomeUser.php
+++ front/welcomeUser.php
@@ -5,10 +5,7 @@ echo $username;
 
 function welcomeUser($userName, $userFirstname){
   if(!$userName){
-     echo '<div class="text-danger" style="text-align: right; background-color: #FFFACD; border-radius: 15px; margin-left: 1600px;">
-     <div class="event" id="creationDeCompte" style="cursor:pointer; font-size: 30px; padding-bottom: 10px;">creer un compte</div>
-     <div class="event" id="connexion" style="cursor:pointer; font-size: 30px; padding-bottom: 10px;">se connecter</div>
-   </div>';
+     echo '<div class="text-danger" style="text-align: right; background-color: #FFFACD; border-radius: 15px; margin-left: 1600px;"><div class="event" id="creationDeCompte" style="cursor:pointer; font-size: 30px; padding-bottom: 10px;">creer un compte</div><div class="event" id="connexion" style="cursor:pointer; font-size: 30px; padding-bottom: 10px;">se connecter</div></div>';
      ?><script>sessionStorage.removeItem('Role');</script><?php
   }else{
      echo "<div><h3 style ='font-size: 30px;'>connecté en temps que ".$userFirstname." ".$userName."</h3><p id='deconnexion'>se deconnecter</p></div>";
