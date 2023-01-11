04153683d85413584e68daa8059d9bcadb98d991
zozo222 <doerrzoran@gmail.com>
2023-01-11 09:44:26 +0100

    re

--- front/listeProduits.php
+++ front/listeProduits.php
@@ -18,33 +18,33 @@ if($userID){
 <html>
   <body>
     <script>
-    console.log(user);
     var data = sessionStorage.getItem('Cle');
     var newArr = JSON.parse(data);
     function tab(newArr){
       for(let i = 0; i < newArr.length; i++){
-        var arr = newArr[i];
-        var image = arr['picture'];
-        var picture = '<img style="vertical-align: top;" class="text-justify figure-img  img-fluid img-thumbnail" alt="Responsive image" src="'+ image +'"width="300" length="300" style="display: inline-block">';
-        document.write('<div style="padding-top: 50px;"><table><tr><th>' + arr['label'] + ' ' +'</th></tr><tr><td>' + picture + '</td>' + '<td><p>' + '</p>' + '<p>' + arr['description'] + ' ' + '</p>' + '<p>' + arr['price'] + '€ ' + '</p></td></tr></table></div>');
-        var purchase = '<form action="../back/scriptCart.php" method="POST" enctype="multipart/form-data"><input id="id" type="hidden" name="product" value="' + arr['id'] + '"><button type ="submit">ajouter ce produit à votre panier</button></form>';
-        if(user == 0){
-          document.write('Veuillez vous connecter pour effectuer un achat');
-        }
-        if(user > 0 && user != arr['seller']){
-          document.write(purchase);
-        }
-        if(user == arr['seller']){
-          document.write('votre article ');
-        }
+  var arr = newArr[i];
+  var image = arr['picture'];
+  var picture = '<img style="vertical-align: top;" class="text-justify figure-img  img-fluid img-thumbnail" alt="Responsive image" src="'+ image +'"width="300" length="300" style="display: inline-block">';
+  document.write('<div style="padding-top: 50px;"><table><tr><th>' + arr['label'] + ' ' +'</th></tr><tr><td>' + picture + '</td>' + '<td><p>' + '</p>' + '<p>' + arr['description'] + ' ' + '</p>' + '<p>' + arr['price'] + '€ ' + '</p></td></tr></table></div>');
+  var purchase = '<form action="../back/scriptCart.php" method="POST" enctype="multipart/form-data"><input id="id" type="hidden" name="product" value="' + arr['id'] + '"><button type ="submit">ajouter ce produit à votre panier</button></form>';
+  if(user == 0){
+    document.write('Veuillez vous connecter pour effectuer un achat');
+  }
+  if(user > 0 && user != arr['seller']){
+    document.write(purchase);
+  }
+  if(user == arr['seller']){
+    document.write('votre article ');
+  }
       }
     }
     </script>
-    <?php  
-    $variable = "<script>tab(newArr);</script>";
-    echo $variable ;
-    ?>
-  </body>
+    <div class="text-primary" style="text-align: left; background-color: #FFFACD; border-radius: 15px; margin-right: 1700px; font-size: 30px;">
+      <?php  
+      $variable = "<script>tab(newArr);</script>";
+      echo $variable ;
+      ?>
+    </div>
   <?php
      require_once "footer.html"; 
   ?>
