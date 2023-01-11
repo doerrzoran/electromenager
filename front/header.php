d84060de90cc08dfdd5f5fff3cf82c88c7229b2b
zozo222 <doerrzoran@gmail.com>
2023-01-11 08:09:09 +0100

    amelioration présentation

--- front/header.php
+++ front/header.php
@@ -5,7 +5,7 @@
         <link rel="stylesheet" type="text/css" href="../Assets/css/bootstrap.css">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
-    <body class="bg-warning" style="padding: 30px;">
+    <body class="bg-warning" style="padding: 30px background-image: url('https://cdn-g8hn2yqg.files-simplefileupload.com/static/blobs/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBNU4yQVE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--e6e3a5c0ef3b85e0e51f6f9bdf082dbccad4f140/IMG_20230109_103505.jpg');;">
       <div class="page-header">
         <header class="p-3 mb-2 bg-info text-white" style="padding-bottom: 30px;">
            <div class="text-danger" style = ''>
