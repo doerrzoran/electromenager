99cbbdd51acf738c4c46bdceb366e5f28512df5b
zozo222 <doerrzoran@gmail.com>
2023-01-11 08:40:34 +0100

    re

--- front/pageAccueil.php
+++ front/pageAccueil.php
@@ -4,15 +4,15 @@
     require_once "header.php";
 ?>
         <div class="text-danger" style="text-align: right; background-color: blue; margin-left: 1700px;">
-          <div class="event" id="creationDeCompte" style="cursor:pointer; font-size: 20px; padding-bottom: 10px;">creer un compte</div>
-          <div class="event" id="connexion" style="cursor:pointer; font-size: 20px; padding-bottom: 10px;">se connecter</div>
+          <div class="event" id="creationDeCompte" style="cursor:pointer; font-size: 30px; padding-bottom: 10px;">creer un compte</div>
+          <div class="event" id="connexion" style="cursor:pointer; font-size: 30px; padding-bottom: 10px;">se connecter</div>
         </div>
         <div class="text-primary">
-          <div class="event" id="utilisateur"  style="cursor:pointer; font-size: 20px; padding-bottom: 10px;">consulter votre page</div>
-          <div class="event" id="vente" style="cursor:pointer; font-size: 20px; padding-bottom: 10px;">vendre</div>
-          <div class="event" id="produits" style="cursor:pointer; font-size: 20px; padding-bottom: 10px;">consulter les articles</div>
-          <div class="event" id="utilisateurs" style="cursor:pointer; font-size: 20px; padding-bottom: 10px;">consulter la liste des utilisateurs</div>
-          <div class="event" id="administrateur" style="cursor:pointer; font-size: 20px; padding-bottom: 10px;">ajouter un nouvel administrateur</div>
+          <div class="event" id="utilisateur"  style="cursor:pointer; font-size: 30px; padding-bottom: 10px;">consulter votre page</div>
+          <div class="event" id="vente" style="cursor:pointer; font-size: 30px; padding-bottom: 10px;">vendre</div>
+          <div class="event" id="produits" style="cursor:pointer; font-size: 30px; padding-bottom: 10px;">consulter les articles</div>
+          <div class="event" id="utilisateurs" style="cursor:pointer; font-size: 30px; padding-bottom: 10px;">consulter la liste des utilisateurs</div>
+          <div class="event" id="administrateur" style="cursor:pointer; font-size: 30px; padding-bottom: 10px;">ajouter un nouvel administrateur</div>
         </div>
         
         <script>
