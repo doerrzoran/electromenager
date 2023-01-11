4f65235c0d5556eb51261d82617c11f3f5155d96
zozo222 <doerrzoran@gmail.com>
2023-01-11 20:53:30 +0100

    re

--- front/header.php
+++ front/header.php
@@ -5,7 +5,7 @@
         <link rel="stylesheet" type="text/css" href="../Assets/css/bootstrap.css">
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
-    <body class="bg-warning img-fluid" style="width 100%; height: auto; padding: 30px; background-image: url('https://cdn-g8hn2yqg.files-simplefileupload.com/static/blobs/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBM3A2QVE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--10ad427bf304ccb04722f1d1a0b3ad0f79ba73b8/l-electromenager-discret-devient-design-dans-la-cuisine_5329043.webp');">
+    <body class="bg-warning img-fluid" style="max-width: 100%; height: auto; padding: 30px; background-image: url('https://cdn-g8hn2yqg.files-simplefileupload.com/static/blobs/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBM3A2QVE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--10ad427bf304ccb04722f1d1a0b3ad0f79ba73b8/l-electromenager-discret-devient-design-dans-la-cuisine_5329043.webp');">
         <header class="page-header p-3 mb-2 bg-info text-white" style="padding-bottom: 30px; border-radius: 15px;">
            <div class="text-danger" style = ''>
              <h1 class="event" id="title" style="cursor:pointer; font-size: 45px;">Electromenager</h1>
