567dcdf6578b795a1c9fad981ce91c03f4f24ae6
zozo222 <doerrzoran@gmail.com>
2023-01-11 11:27:01 +0100

    re

--- back/deleteProduct.php
+++ back/deleteProduct.php
@@ -5,14 +5,13 @@ require_once 'userId.php';
 $user = $userID;
 
 $product = $_POST['product'];
-echo $product;
 
-// //suppression panier
-// $cart = selectFromDatabase("_cart", "client", $userID, $conn);
-// $cartID = $cart[0];
-// $deletionCart = deleteFromDatabase("_cart", "id", $cartID, $conn);
+//suppression panier
+$cart = selectFromDatabase("_cart", "client", $userID, $conn);
+$cartID = $cart[0];
+$deletionCart = deleteFromDatabase("_cart", "id", $cartID, $conn);
 
-// //suppression produit
-// $deletion = deleteFromDatabase("_product", "id", $product, $conn);
+//suppression produit
+$deletion = deleteFromDatabase("_product", "id", $product, $conn);
 
-// header("Location: DisplaySeller.php");
+header("Location: DisplaySeller.php");
