0729fbdfaf682e0e3a5428f9c4e7cb630a1f6687
zozo222 <doerrzoran@gmail.com>
2023-01-11 10:48:44 +0100

    re

--- front/header.php
+++ front/header.php
@@ -6,13 +6,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body class="bg-warning" style="padding: 30px; background-image: url('https://cdn-g8hn2yqg.files-simplefileupload.com/static/blobs/proxy/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBNU4yQVE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--e6e3a5c0ef3b85e0e51f6f9bdf082dbccad4f140/IMG_20230109_103505.jpg');">
-      <div class="page-header">
-        <header class="p-3 mb-2 bg-info text-white" style="padding-bottom: 30px; border-radius: 15px;">
+        <header class="page-header p-3 mb-2 bg-info text-white" style="padding-bottom: 30px; border-radius: 15px;">
            <div class="text-danger" style = ''>
              <h1 class="event" id="title" style="cursor:pointer; font-size: 45px;">Electromenager</h1>
            </div>
         </header>
-       </div>
         <div class="text-danger" style = "text-align: right; font-size: 30px; background-color: #FFFACD; border-radius: 15px; margin-left: 1300px;">
         <?php
           require_once "welcomeUser.php";
