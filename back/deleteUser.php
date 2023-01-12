4b17d15921f4a65310d381bbb07723eb470df0ce
zozo222 <doerrzoran@gmail.com>
2023-01-12 10:48:40 +0100

    correction suppression utilisateur

--- back/deleteUser.php
+++ back/deleteUser.php
@@ -4,20 +4,16 @@ require_once 'dbbFunctions.php';
 
 $id = $_POST['id'];
 
-$products = selectFromDatabase("_product", "seller", $id, $conn);
+//suppression du panier
+$cart = deleteFromDatabase("_cart", "client", $id, $conn);
 
-$product = $products[0];
+//suppression du produit
+$productID = deleteFromDatabase("_product", "seller", $id, $conn);
 
+
+
+// suppression de l'utilisateur
 $deletionUser = deleteFromDatabase("user_acount", "id", $id, $conn);
-$cart = selectFromDatabase("_cart", "client", $id, $conn);
-$cartID = $cart[0];
-$deletionCart = deleteFromDatabase("_cart", "id", $cartID, $conn);
-
-$productID = selectFromDatabase("_product", "seller", $id, $conn);
-$productID = $productID[0];
-
-$deletionProduct = deleteFromDatabase("_product", "id", $productID, $conn);
-
 
 
 
